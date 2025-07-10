dc6c366b0b9bdd21a4c96aa7063dcf25dfc64d39
Peppeh06 <giuseppegrosso450@gmail.com>
2025-07-10 21:36:54 +0200

    Add files via upload

---
+++ dentista-master/dentista-master/js/leaflet-map.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    if(document.getElementById("map")) {
+        const map = L.map("map").setView([41.2289786, 16.490343], 15);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        const dentalIcon = L.icon({
+            iconUrl: 'img/ping.svg',
+            iconSize: [32, 32],
+            iconAnchor: [16, 32],
+            popupAnchor: [0, -32]
+        });
+
+        L.marker([41.2289786, 16.490343], {icon: dentalIcon})
+            .addTo(map)
+            .bindPopup('Studio Dentistico Sant\'Andrea<br> Via don Tonino Bello, 5')
+            .openPopup();
+    }
+})
